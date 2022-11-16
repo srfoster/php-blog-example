c5a5d9b375960f83f7977bee3579b8b52a36e013
Stephe Foster <sfoster@nsacademic.edu>
2022-11-16 09:23:49 -0800

    closes #16

--- database/migrations/0001-add-titles-to-posts.sql
+++ database/migrations/0001-add-titles-to-posts.sql
@@ -1,3 +1,9 @@
 
+USE blog;
+
 -- Change posts table to have a title column
 ALTER TABLE posts ADD title VARCHAR(255);
+
+-- Migrate any seed blogData
+UPDATE posts SET title = "Hello" WHERE id = 1;
+UPDATE posts SET title = "Hello2" WHERE id = 2;
--- database/migrations/0002-add-profile-pic-to-users.sql
+++ database/migrations/0002-add-profile-pic-to-users.sql
@@ -1,3 +1,9 @@
 
+USE blog;
+
 -- Change users table to have a profile_pic_url column
 ALTER TABLE users ADD profile_pic_url TEXT;
+
+-- Migrate any seed blogData
+UPDATE users SET profile_pic_url = "https://mui.com/static/images/avatar/1.jpg" WHERE id = 1;
+UPDATE users SET profile_pic_url = "https://mui.com/static/images/avatar/3.jpg" WHERE id = 2;
